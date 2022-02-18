2c46e7c3d8be4a165eab8fc08af41477b5089734
Valentine Garbuzenko <nickf@tut.by>
2022-02-18 16:23:06 +0300

    fix: change iteration loop for review a string

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 // source configs
-// const config1 = [['(', ')']];
-// const config2 = [['(', ')'], ['[', ']']];
-// const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-// const config4 = [['|', '|']];
-// const config5 = [['(', ')'], ['|', '|']];
-// const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 
-// const inStr = '{([{}])}'; // true
-// const curCfg = config3;
+const inStr = '([{}])', curCfg = config3; // 4-true
 // const inStr = '[(])', curCfg = config2; // false
 // const inStr = '|()|(||)||', curCfg = config5; // true
 // const inStr = '8888877878887777777888888887777777887887788788887887777777788888888887788888', curCfg = config6; // false
+// const inStr = '([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())', curCfg = config7; // 19-true
 
-module.exports = function check(str, bracketsConfig) {
+// module.exports = 
+function check(str, bracketsConfig) {
   let resBool = false;
   let inStr = str;
   let arrBrackets = bracketsConfig.map((item) => item.join(""));
   console.log(arrBrackets);
   
-  for (let i = 0; i < inStr.length / 2 + 1 ; i++) {
+  // This loop iterates values (of array) that iterable, as an iterable object, defines to be iterated over. 
+  // The object's elements (of inStr) are shown, but none of the object's properties.
+
+  for (let curChar of inStr) {
     arrBrackets.forEach((item) => { 
       while (inStr.includes(item)) {
-        // console.log(item, inStr); 
+        console.log(item, inStr); 
         inStr = inStr.replace(item, "");
+        console.log(inStr); 
       }
     } );
   }
@@ -49,5 +54,5 @@ module.exports = function check(str, bracketsConfig) {
   return resBool;
 }
 
-// res = check(inStr, curCfg);
-// console.log(res);
+res = check(inStr, curCfg);
+console.log(res);
